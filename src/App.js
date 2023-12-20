54acae13e541d7c7cb453cbe4c2ca5feeeb68e07
SiyalM <siyalmeshram19@gmail.com>
2023-12-20 12:21:12 +0530

    feat:add chat frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Chat from './Components/Chat/Chat';
 
 function App() {
   return (
     <div className="app">
-      <h1>hello</h1>
       <div className="app__body">
           <Sidebar/>
+          <Chat/>
       </div>
     </div>
   );
