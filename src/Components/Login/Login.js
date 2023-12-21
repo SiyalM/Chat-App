317220e4c96e5dae95c1ba2d7d28fc78e134c63d
SiyalM <siyalmeshram19@gmail.com>
2023-12-21 12:28:31 +0530

    feat:add last seen message on room header

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,36 +1,39 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
-import './Login.css';
-import {auth, provider} from '../../firebase';
-import { useStateValue } from '../../StateProvider';
-import { actionTypes } from '../../reducer';
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "../../firebase";
+import { useStateValue } from "../../StateProvider";
+import { actionTypes } from "../../reducer";
 
 function Login() {
-    const [{}, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
-    const signin = () => {
-        auth.signInWithPopup(provider)
-            .then(result => {
-                dispatch({
-                    type: actionTypes.SET_USER,
-                    user: result.user
-                })
-            })
-            .catch(error => console.log(error))
-    }
-    return (
-        <div className="login">
-            <div className="login__container">
-                <img
-                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/225px-WhatsApp.svg.png"/>
-                 <div className="login__text">
-                     <h1>sign in to WhatsApp</h1>
-                 </div>
-
-                 <Button onClick={signin}>Sign in with Google</Button>
-            </div>
+  const signin = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/225px-WhatsApp.svg.png"
+          alt="whatsapp logo"
+        />
+        <div className="login__text">
+          <h1>sign in to WhatsApp</h1>
         </div>
-    )
+
+        <Button onClick={signin}>Sign in with Google</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
